8126134a3a6e4f8daa1dbccd6140c46626656bab
Chuck Hedrick and Syed Nabeel <pair+chuck+syed@allstate.com>
2016-06-17 10:42:51 -0500

    updates to puchase and sell test cases

--- test/stock.spec.js
+++ test/stock.spec.js
@@ -4,14 +4,23 @@ space-before-function-paren, no-traiing-spaces */
 const expect = require('chai').expect;
 const Stock = require('../lib/stock');
 const nock = require('nock');
+const sinon = require('sinon');
+let clock;
+
 describe('Stock', () => {
   beforeEach(() => {
+    clock = sinon.useFakeTimers();
+    clock.tick(150);
     nock(`http://dev.markitondemand.com`)
     .get(`/MODApis/Api/v2/Quote/json?symbol=AAPL`)
     .reply(200, {
       Name : 'Apple',
       LastPrice: 100,
     });
+  });
+  after(() => {
+    nock.restore();
+    clock.restore(); //restore it old timer
   });
   describe('constructor', () => {
     it('should construct a new Stock object', () => {
@@ -23,11 +32,12 @@ describe('Stock', () => {
   describe('#purchase', () => {
     it('should purchase stock', (done) => {
       const s1 = new Stock('aapl');
+
       s1.purchase(50, (err, totalPaid) => {
         expect(err).to.be.null;
         expect(totalPaid).to.equal(5000);
         expect(s1.shares).to.equal(50);
-        //expect(s1.purchaseDate.getTime()).to.be.equal();
+        expect(s1.purchaseDate.getTime()).to.be.equal(150);
         expect(s1.name).to.equal('Apple');
         expect(s1.purchasePricePerShare).to.equal(100);
         done();
@@ -38,9 +48,12 @@ describe('Stock', () => {
   describe('#sell', () => {
     it('should sell stock', (done) => {
       const s2 = new Stock('aapl');
+      s2.shares = 50;
       s2.sell(30, (err, totalCashRefund) => {
+
         expect(err).to.be.null;
-        expect(totalCashRefund).to.be.above(2500);
+        expect(s2.shares).to.equal(20);
+        expect(totalCashRefund).to.be.equal(3000);
         expect(s2.name).to.equal('Apple');
         expect(s2.sellPricePerShare).to.be.above(0);
         done();
@@ -49,11 +62,10 @@ describe('Stock', () => {
 
     it('should not sell stock', (done) => {
       const s2 = new Stock('aapl');
+      s2.shares=50;
       s2.sell(100, (err, totalCashRefund) => {
-      //  expect(err).to.be.null;
-        expect(totalCashRefund).to.be.above(2500);
-        expect(s2.name).to.equal('Apple');
-        expect(s2.sellPricePerShare).to.be.above(0);
+        expect(totalCashRefund).to.equal(0);
+        expect(err).to.be.instanceOf(Error);
         done();
       });
     });
